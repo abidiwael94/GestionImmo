b234493bb4299e8b92ee727bc80229a38be3b9f8
AnIscoder01 <anisrojo519@gmail.com>
2025-05-21 21:58:20 +0100

    all is good

--- Migrations/20250521205744_migration2.Designer.cs
+++ Migrations/20250521205744_migration2.Designer.cs
@@ -4,6 +4,7 @@ using GestionImmo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GestionImmo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250521205744_migration2")]
+    partial class migration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250521205744_migration2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GestionImmo.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FullName", "Role", "address", "email", "password", "phone" },
+                values: new object[,]
+                {
+                    { new Guid("3ab35ede-7489-49f6-af05-f7043cd74093"), "admin S", 0, "Tunis", "se@admin.com", "password123", "20202020" },
+                    { new Guid("95bd3141-36a9-4dd6-adf1-291875bb8c83"), "agent S", 1, "Ariana", "s@eagent.com", "password456", "30303030" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("3ab35ede-7489-49f6-af05-f7043cd74093"));
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("95bd3141-36a9-4dd6-adf1-291875bb8c83"));
+        }
+    }
+}
